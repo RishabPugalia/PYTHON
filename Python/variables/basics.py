78e182f053c2274a1f9ab5a34f44a2e28587c69f
RishabPugalia <rishabpugalia23@gmail.com>
2025-06-25 18:11:53 +0530

    Add files via upload

---
+++ Python/variables/basics.py
@@ -0,0 +1,5 @@
+age = 21 #integer
+name = "John Doe" #string
+cgpa = 3.8 #float
+
+_abc_c = 10 #variable with underscore
---
+++ Python/variables/datatypes.py
@@ -0,0 +1,20 @@
+# This script demonstrates different data types in Python.
+#integer variable
+age = 34
+print(age)
+print(type(age))  # Output: <class 'int'>
+
+#string variable
+name = "Alice"
+print(name)
+print(type(name))  # Output: <class 'str'>
+
+#float variable
+cgpa = 3.9
+print(cgpa)
+print(type(cgpa))  # Output: <class 'float'>
+
+#boolean variable
+is_student = True # can be True or False
+print(is_student)
+print(type(is_student))  # Output: <class 'bool'>
