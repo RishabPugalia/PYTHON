3907028c92b4cd5277e0b5ed0c1938d46de18e3b
RishabPugalia <rishabpugalia23@gmail.com>
2025-06-26 21:56:50 +0530

    Add files via upload

---
+++ Python/userinput/1_input.py
@@ -0,0 +1,9 @@
+a = input("Enter a number: ")
+print("You entered:", a)
+
+b = input("Enter name: ")
+print("Your name is:", b)
+
+# This script takes user input for a number and a name, then prints them.
+# It demonstrates the use of the input function to capture user input.
+
---
+++ Python/userinput/2_add.py
@@ -0,0 +1,23 @@
+# a = input("Enter 1st number: ")
+# a = int(a)  # Convert input to integer
+
+# print(a + 3)
+
+# 1ST METHOD
+
+# a = input("Enter 1st number: ")
+# b = input("Enter 2nd number: ")
+
+# # Convert inputs to integers
+# a = int(a)
+# b = int(b)
+# # Calculate the sum
+# sum_result = a + b
+# # Print the result
+# print("The sum of", a, "and", b, "is:", sum_result)
+
+#2ND METHOD
+a = int(input("Enter 1st number: "))
+b = int(input("Enter 2nd number: "))
+
+print(a + b)
