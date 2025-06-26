9646f019a56888d20d618b933e97ec1f1de589e3
RishabPugalia <rishabpugalia23@gmail.com>
2025-06-26 22:00:49 +0530

    Add files via upload

---
+++ Python/operators/operators.py
@@ -0,0 +1,51 @@
+# a = 34
+# b = 2
+
+# Arithmetic operations
+# print("a + b =", a + b)  # Addition
+# print("a - b =", a - b)  # Subtraction
+# print("a * b =", a * b)  # Multiplication
+# print("a / b =", a / b)  # Division
+# print("a // b =", a // b)  # Floor Division
+# print("a % b =", a % b)  # Modulus
+# print("a ** b =", a ** b)  # Exponentiation
+
+# Conditional operators
+# print(a>4)
+# print(a<4)
+# print(a>=4)
+# print(a<=4)
+# print(a==4) #is equal to 4?
+# print(a!=4) #is not equal to 4?
+
+# Logical operators
+# c = True
+# d = False
+# print("AND")
+# print(c and c)
+# print(c and d)
+# print(d and c)
+# print(d and d)
+# print("OR")
+# print(c or c)
+# print(c or d)
+# print(d or c)
+# print(d or d)
+# print("NOT")
+# print(not c)  
+# print(not d)  
+
+# Assignment operators
+# a = 32
+# print(a)
+# a+=3 # a = a + 3(- / * / % // **)
+# a-=3 # a = a - 3
+# a*=3 # a = a * 3
+# a/=3 # a = a / 3
+# a//=3 # a = a // 3
+# a%=3 # a = a % 3
+# print(a) 
+
+
+
+
